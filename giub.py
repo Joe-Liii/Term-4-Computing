2435b866c3ac344bbd8e72985251864e91c03cb2
LyraL <you@example.com>
2026-09-21 09:14:13 +1000

    submit 1

---
+++ giub.py
@@ -0,0 +1,11 @@
+import os
+def clear_screen():
+    _ = os.system('cls')
+
+clear_screen()
+import time
+counter=range(10,1)
+for a in range (10, 1, -1):
+    print(a)
+    time.sleep(1)
+print ("bah")
